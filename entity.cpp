a21d6ff967e511f16f9359da528ebeece5772a02
yonjoos <121422885+yonjoos@users.noreply.github.com>
2023-05-26 08:44:39 +0900

    ver4
    사용자의 지원삭제, 조회 추가

---
+++ entity.cpp
@@ -0,0 +1,333 @@
+#include "entity.h"
+#include "boundary.h"
+
+
+
+
+void doTask(){
+    
+    MemberList members;
+    RecruitInfoList recruits;
+    Member* pNewMember = NULL;
+    string currentID  = "";
+    string currentPW = "";
+    
+    int menu_level_1 = 0, menu_level_2=0;
+    int is_program_exit = 0;
+    
+    while(!is_program_exit){
+        
+        cout << "1.1 회원 가입\n";
+        cout << "1.2 회원 탈퇴\n";
+        cout << "2.1 로그인\n";
+        cout << "2.2 로그아웃\n";
+        cout << "3.1 채용정보 등록\n";
+        cout << "3.2 등록된 채용정보 좋회\n";
+        cout << "4.1 채용 정보 검색\n";
+        cout << "4.2 채용 지원\n";
+        cout << "4.3 지원 정보 조회\n";
+        cout << "5.1 지원 정보 통계\n";
+        cout << "6.1 종료\n";
+        
+        cin >> menu_level_1;
+        cin.ignore();
+        cin >> menu_level_2;
+        switch(menu_level_1){
+            case 1:
+            {
+                Control_Register* control_Reg = new Control_Register();
+                switch(menu_level_2){
+                    case 1: //1.1 회원가입, 1 hankook 3456 hk pwd1 /OR/ 2 98765 hn pwd2
+                    {
+                        
+                        control_Reg->call_startInterface(&members);
+                        members.showAll(); //왜 안 되냐??
+                        break;
+                    }
+                    case 2://1.2 회원 탈퇴
+                    {
+                        Control_DeleteAccount* control_deleteAccount = new Control_DeleteAccount();
+                        control_deleteAccount->call_startInterface(&members, &currentID, &currentPW);
+                        break;
+                    }
+                }
+                break;
+            }
+            case 2:
+            {
+                Control_Login* control_log = new Control_Login();
+                switch(menu_level_2){
+                    case 1: //2.1 로그인, hk pwd1
+                    {
+                        control_log->call_startInterface(&members, 1, &currentID, &currentPW);
+                        break;
+                    }
+                    case 2: //2.2 로그아웃, hk
+                    {
+                        control_log->call_startInterface(&members, 0, &currentID, &currentPW);
+                    }
+                }
+                break;
+            }
+            case 3:
+            {
+                Control_ApplyRecruit* control_applyrec = new Control_ApplyRecruit();
+                switch(menu_level_2){
+                    case 1: //3.1 채용 정보 등록
+                    {
+                        control_applyrec->call_startInterface(&members, &recruits, &currentID, &currentPW);
+                    }
+                    case 2: //3.2 등록된 채용 정보 조회
+                    {
+                        
+                    }
+                    
+                }
+                break;
+            }
+            case 4:
+            {
+                switch(menu_level_2){
+                    case 1: //4.1 채용 정보 검색
+                    {
+                        Control_UserInfo* control_user = new Control_UserInfo();
+                        control_user->call_startInterface(&members, &recruits, &currentID, &currentPW);
+                    }
+                    case 2: //4.2 채용 지원
+                    {
+                    }
+                    case 3: //4.3 지원 정보 조회
+                    {
+                        
+                    }
+                    case 4: //4.4 지원 취소
+                    {
+                        Control_DeleteApply* control_user = new Control_DeleteApply();
+                        control_user->call_startInterface(&members, &recruits, &currentID, &currentPW);
+                    }
+
+                }
+                break;
+            }
+            case 5:
+            {
+                switch(menu_level_2){
+                    case 1: // 5.1 지원 정보 통계
+                    {
+                        
+                    }
+                }
+                break;
+            }
+            case 6:
+            {
+                switch(menu_level_2){
+                    case 1: // 6.1 종료
+                    {
+                        program_exit();
+                        is_program_exit = 1;
+                        break;
+                        
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+// join()
+void join(){
+    
+}
+
+
+void program_exit(){
+    cout << "sibal";
+}
+
+//****************************************************************************************************
+//****************************************************************************************************
+//****************************************************************************************************
+
+
+
+//============================ Member Entity 관련 함수 ============================
+// Member :: setType()
+void Member::setType(int type)
+{
+    if(type == 1) type = 1; //회사 회원
+    else type = 0; //일반 회원
+}
+
+
+
+// Member :: setState
+void Member::setState(int _state){
+    if(_state == 1){
+        state = 1;
+    }
+    else if(_state == 0){
+        state = 0;
+    }
+    else {}
+}
+
+
+void UserMember::sortList(){
+    sort(AppliedList, AppliedList+appliedNum);
+}
+
+string UserMember::getCompname(int index){
+    return AppliedList[index]->getCompName();
+}
+string UserMember::getTask(int index){
+    return AppliedList[index]->getTask();
+}
+int UserMember::getHeads(int index){
+    return AppliedList[index]->getHeads();
+}
+int UserMember::getDeadline(int index){
+    return AppliedList[index]->getDeadline();
+}
+
+void UserMember::Apply(RecruitInfo* rec){
+    AppliedList[appliedNum++] = rec;
+}
+
+
+
+// <<<<<<<<<<<<<<<<<<<<< FOR DEBUGGING >>>>>>>>>>>>>>>>>>>>>>>>
+// Member :: show()
+void Member::show(){
+    cout << "\nID: " << ID << "\n" << "PW: " << PW << "\n\n" << endl;
+}
+
+
+
+
+//============================ RecruitInfoList 관련 함수 ============================
+string RecruitInfo::getCompName(){
+    return CompanyName;
+}
+string RecruitInfo::getTask(){
+    return task;
+}
+int RecruitInfo::getHeads(){
+    return applyNumber;
+}
+int RecruitInfo::getDeadline(){
+    return deadline;
+}
+
+
+
+void RecruitInfoList::addRecruitInfo(RecruitInfo* recInfo){
+
+    //RecruitInfo(string _compname, int _Bn, string _task, int _applynum, int _deadline):taskApplied(0){
+
+    recruitlist[numRecruitInfo++] = recInfo;
+}
+
+
+//============================ MemberList 관련 함수 ============================
+
+void UserMember::showAllApplied(){
+    for(int i = 0; i < appliedNum; i++){
+        cout << "[ "  << i << " ]\n";
+        cout << getCompname(i) <<"\n";
+        cout << getTask(i) << "\n";
+        cout << getHeads(i) << "\n";
+        cout << getDeadline(i) << "\n\n\n";
+    }
+}
+
+
+
+
+
+//MemberList :: addMember
+void MemberList::addMember(Member* member){ //이거 포인터로 전달하기 실수!!
+    memberList[numMembers++] = member;
+}
+
+
+//MemberList :: deleteMember
+void MemberList::deleteMember(string _name){
+    for(int i= 0; i < numMembers; i++){
+        if(memberList[i]->Names()){
+            memberList[i]->Names();
+        }
+    }
+}
+
+
+//MemberList :: checkIDlist
+int MemberList::checkIDlist(string id, string pw){
+    for(int i = 0; i < this->getnumber(); i++){
+        if(memberList[i]->getID() == id){
+            if(memberList[i]->getPW() == pw){
+                //cout << "로그인 성공";
+                return i; //인덱스를 반환
+            }
+        }
+    }
+    return -1;
+}
+
+
+//MemberList :: setState
+void MemberList::setState(int _state, int index){
+    memberList[index]->setState(_state);
+}
+
+
+//MemberList :: getState
+int MemberList::getState(int index){
+    return memberList[index]->getState(); // 로그인 상태 반환
+}
+
+UserMember* MemberList::getMember(int index){
+    return memberList[index]->getUserMember();
+}
+
+
+
+void MemberList::setNumMembers(int num){
+    numMembers = num;
+}
+
+
+
+// <<<<<<<<<<<<<<<<<<<<< FOR DEBUGGING >>>>>>>>>>>>>>>>>>>>>>>>
+//MemberList :: showAll()
+void MemberList::showAll(){
+    for (int i = 0; i < numMembers; i++){
+        memberList[i]->show();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ entity.h
@@ -0,0 +1,296 @@
+#ifndef software_h
+#define software_h
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+
+// **************************************************************************
+// **************************************************************************
+// 상수 선언
+#define MAX_STRING 32
+#define INPUT_FILE_NAME "input.txt"
+#define OUTPUT_FILE_NAME "output.txt"
+
+using namespace std;
+
+
+// **************************************************************************
+// **************************************************************************
+// 함수 선언
+void doTask();
+void join();
+void program_exit();
+
+
+// **************************************************************************
+// **************************************************************************
+// 클래스 선언
+
+
+class RecruitInfo;
+class MemberList;
+class UserMember;
+class CompanyMember;
+
+
+// ===== Memmber class
+class Member{
+private:
+    string ID;
+    string PW;
+    int type = -1; //회사 = 1, 일반 = 0
+    int state = -1; // 로그인 = 1
+    
+public:
+    Member(int _t, string _id, string _pw):ID(_id), PW(_pw), type(1){}
+    
+    void setID(string _ID){ID = _ID;}
+    void setPW(string _PW){PW = _PW;}
+    void setType(int type);
+    void setState(int _state);
+    
+    
+    virtual void saveInfo() = 0;
+    virtual void deleteInfo()= 0;
+    virtual bool checkID()= 0;
+    virtual void addApplicant()= 0;
+    virtual void listApply()= 0;
+    virtual void cancleApply()= 0; // 회원 함수 : 지원을 취소
+    virtual void listStatUser()= 0; //
+    virtual int Names() = 0;
+    void show();
+    string getID(){return ID;}
+    string getPW(){return PW;}
+    virtual string getName()=0;
+    virtual int getNumber()=0;
+    int getState(){return state;}
+    
+    virtual UserMember* getUserMember()=0;
+
+
+};
+
+
+
+// ===== User class : Member
+class UserMember : public Member //일반 회원
+{
+private:
+    //ID
+    //PWD 는 상속
+    string name;
+    int RegisterationNumber; //주민번호
+    RecruitInfo* AppliedList[10];
+    int appliedNum = 0;
+    
+public:
+    UserMember(int type, int _serial,  string _ID, string _PW):Member(type, _ID, _PW){
+        this->RegisterationNumber = _serial;
+    }
+    virtual void saveInfo(){};
+    virtual void deleteInfo(){};
+    virtual bool checkID(){return true;};
+    virtual void addApplicant(){};
+    virtual void listApply(){};
+    virtual void cancleApply(){}; // 회원 함수 : 지원을 취소
+    virtual void listStatUser(){}; //
+    virtual int Names(){return 0;}
+    virtual string getName(){return name;}
+    virtual int getNumber(){return RegisterationNumber;}
+    
+    void sortList();
+    string getCompname(int index);
+    string getTask(int index);
+    int getHeads(int index);
+    int getDeadline(int index);
+    int getAppliedNum(){return appliedNum;}
+    virtual UserMember* getUserMember(){return this;}
+    void showAllApplied();
+    void Apply(RecruitInfo* rec);
+    RecruitInfo* getAppliedList(int index){return AppliedList[index];}
+
+    void setAppliedNum(int num){appliedNum = num;}
+    
+    string* listApply(RecruitInfo* recruitInfo[], int n);
+    string* cancelApply(MemberList* user, int businessNumber1);
+    
+    void listStat(MemberList* member);
+
+};
+
+
+
+/*
+// ===== RecruitInfo class
+class RecruitInfo
+{
+private:
+    string CompanyName; // 1) 회사이름
+    int BusinessNumber; // 2) 사업자 번호
+    string task; // 3) 업무
+    int applyNumber; // 4) 인원수
+    int deadline; //5) 신청 마감일
+    int taskApplied;
+    
+public:
+    RecruitInfo(string _compname, int _Bn, string _task, int _applynum, int _deadline):taskApplied(0){
+        CompanyName = _compname;
+        BusinessNumber = _Bn;
+        task = _task;
+        applyNumber = _applynum;
+        deadline = _deadline;
+    }
+    //void getDetail(); //XXX (UI_showDetail, getDetail 삭제 해야함)
+    //void addApply(); // 즉시 지원( 외부_selectApply(), UI_apply(), addApply()
+    //void getRecruitInfoList();
+    //void editRecruitInfoList(); //XXX
+    //void deleteRecruitInfoList(); //XXX
+    //void checkAppNum();
+    //void deleteApplyList(); // XXX
+    //void getApply();
+    //void getCompList();
+    //void getTaskList();
+    
+    //=======김다빈================
+ 
+     string getTask();
+     int getBusinessNumber();
+     string getCompanyName();
+     string getDeadline();
+     void cancelApplyList(int a, int b);
+     int getApplyNumber();
+ 
+};
+*/
+
+
+
+
+class RecruitInfo
+{
+private:
+    string CompanyName; // 1) 회사이름
+    int BusinessNumber; // 2) 사업자 번호
+    string task; // 3) 업무
+    int applyNumber; // 4) 인원수
+    int deadline; //5) 신청 마감일
+    int taskApplied;
+ 
+    
+public:
+    RecruitInfo(){taskApplied = 0;}
+    RecruitInfo(string compname, int bn, string _task, int an, int _deadline){
+        CompanyName = compname;
+        BusinessNumber = bn;
+        task = _task;
+        applyNumber = an;
+        deadline = _deadline;
+        taskApplied = 0;
+    }
+    
+    string getCompName();
+    string getTask();
+    int getHeads();
+    int getDeadline();
+
+};
+
+
+
+
+// =====companyStaff class : Member
+class CompanyMember : public Member
+{
+private:
+    //ID
+    //PWD
+    string companyName;
+    int BusinessNumber;
+    RecruitInfo lists[50];
+    
+public:
+    CompanyMember(int _type, string _companyName, int _businessNum, string _ID, string _PW):Member(_type, _ID, _PW){
+        this->companyName = _companyName;
+        this->BusinessNumber = _businessNum;
+    }
+    
+    virtual void saveInfo(){};
+    virtual void deleteInfo(){};
+    virtual bool checkID(){return true;};
+    virtual void addApplicant(){};
+    virtual void listApply(){};
+    virtual void cancleApply(){}; // 회원 함수 : 지원을 취소
+    virtual void listStatUser(){}; //
+    virtual int Names(){
+        cout << this->companyName;
+        return 1;
+    }
+     
+     
+    virtual string getName(){return companyName;}
+    virtual int getNumber(){return BusinessNumber;}
+    virtual UserMember* getUserMember(){
+        UserMember* user = new UserMember(1, 2, "1", "3");
+        return user;
+    }
+};
+
+
+
+// ===== MemberList class
+class MemberList
+{
+private:
+    Member* memberList[50];
+    int numMembers;
+    
+public:
+    MemberList():numMembers(0){};
+    void addMember(Member* member);
+    void deleteMember(string _name);
+    void showAll();
+    int getnumber(){return numMembers;}
+    int checkIDlist(string, string);
+    void setState(int _state, int index);
+    int getState(int index);
+    
+    void setNumMembers(int num);
+    UserMember* getMember(int index);
+    
+};
+
+
+
+
+
+
+
+class RecruitInfoList
+{
+private:
+    RecruitInfo* recruitlist[50];
+    int numRecruitInfo;
+    
+public:
+    RecruitInfoList():numRecruitInfo(0){}
+    void addRecruitInfo(RecruitInfo* recruitinfo);
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* software_h */
